7b6224b170640eade5c029d23d0c9e1e50e67f4c
Tatiana <tatianasstavares@gmail.com>
2020-11-25 15:09:25 +0000

    Fix identation

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const express = require('express');
-const nunjucks = require('nunjucks')
-const routes = require("./routes")
+const nunjucks = require('nunjucks');
+const routes = require("./routes");
 
-const server = express()
+const server = express();
 
+server.use(express.static('public'));
+server.use(routes);
 
-server.use(express.static('public'))
-server.use(routes)
-
-server.set("view engine", "njk")
+server.set("view engine", "njk");
 
 nunjucks.configure("view", {
     express: server,
